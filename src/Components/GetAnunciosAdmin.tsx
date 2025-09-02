18f4e5bb7f121f02615018a4017f9487ded81a0a
gabriel <s2gb77@gamil.com>
2025-09-02 17:29:17 -0400

    fix: card

--- src/Components/GetAnunciosAdmin.tsx
+++ src/Components/GetAnunciosAdmin.tsx
@@ -203,7 +203,7 @@ export default function GetAnunciosAdmin({ selectedDuration = '2', onFetchAnunci
                       console.log('✅ Imagem carregada com sucesso:', anuncio.image);
                     }}
                   />
-                  <div className="absolute inset-0 flex items-center justify-center bg-gray-200 text-gray-500 text-sm">
+                  <div className="hiddenabsolute inset-0 items-center justify-center bg-gray-200 text-gray-500 text-sm">
                     Sem imagem
                   </div>
                 </div>
