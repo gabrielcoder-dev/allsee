081a85bb5bce5dfc07ff7fed0736edd799d6c94b
gabriel <s2gb77@gamil.com>
2025-09-12 07:16:16 -0400

    fix: modal

--- src/app/(private)/meus-anuncios/page.tsx
+++ src/app/(private)/meus-anuncios/page.tsx
@@ -189,15 +189,20 @@ const MeusAnuncios = () => {
         </div>
       )}
       {isModalOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 bg-opacity-50">
           <div className="bg-white p-4 rounded-lg">
             <h2 className="text-lg font-semibold mb-2">Trocar Arte</h2>
-            <input type="file" />
+            <input
+              type="file"
+              accept="image/*,video/*"
+              id="upload-art"
+              className="hidden"
+            />
             <div className="flex justify-end gap-2 mt-4">
               <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" onClick={() => setIsModalOpen(false)}>
                 Cancelar
               </button>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <button className="bg-orange-600 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded">
                 Trocar
               </button>
             </div>
