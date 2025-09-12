8be02fa3d973b740293657cf567497071b5ee86f
gabriel <s2gb77@gamil.com>
2025-09-11 21:34:11 -0400

    fix: arte

--- src/app/(private)/meus-anuncios/page.tsx
+++ src/app/(private)/meus-anuncios/page.tsx
@@ -3,7 +3,6 @@
 
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "next/router";
 
