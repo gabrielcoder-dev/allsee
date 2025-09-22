707de5c93bff1b17c3e85391482af94d43e464b6
gabriel <s2gb77@gamil.com>
2025-09-22 18:18:25 -0400

    fix: carrosel

--- src/Components/Testimonials.tsx
+++ src/Components/Testimonials.tsx
@@ -39,13 +39,13 @@ const Testimonials = () => {
 
         .carousel-wrapper {
           display: flex;
-          width: 200%; /* Double the width */
-          animation: slide 30s linear infinite; /* Increased animation duration */
+          width: 200%;
+          animation: slide 15s linear infinite;
         }
 
         .carousel-slide {
           display: flex;
-          width: auto; /* Each slide takes 50% */
+          width: 50%;
           gap: 100px; 
         }
 
@@ -59,7 +59,7 @@ const Testimonials = () => {
             transform: translateX(0);
           }
           100% {
-            transform: translateX(-50%); /* Move by 50% */
+            transform: translateX(-50%);
           }
         }
       `}</style>
