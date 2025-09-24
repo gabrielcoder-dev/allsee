ff8972d566e9cde9711bd3c66bc57cb81d0fcd10
gabriel <s2gb77@gamil.com>
2025-09-24 19:06:19 -0400

    fix: cta

--- src/Components/CTA.tsx
+++ src/Components/CTA.tsx
@@ -1,10 +1,13 @@
 export default function Cta() {
   return (
-    <section className="w-full  flex justify-center items-center mt-10 bg-white px-2 sm:px-4" id="contato">
+    <section
+      className="w-full  flex justify-center items-center mt-10 bg-white px-2 sm:px-4"
+      id="contato"
+    >
       <div className="text-center bg-orange-50 w-full max-w-2xl md:max-w-3xl lg:max-w-4xl py-10 md:py-16 px-4 sm:px-8 md:px-10 lg:px-16 rounded-lg flex flex-col items-center gap-6 shadow">
         {/* Subtítulo */}
         <p className="text-orange-600 uppercase font-semibold tracking-widest text-center text-sm md:text-base lg:text-lg mb-3">
-          FAÇA PARTE DA MAIOR EMPRESA DE MARCKETING INDOR DE PRIMAVERA DO LESTE
+          Anuncie na maior empresa de Marketing Indoor de Primavera do Leste
         </p>
 
         {/* Título */}
@@ -23,5 +26,5 @@ export default function Cta() {
         </a>
       </div>
     </section>
-  )
+  );
 }
