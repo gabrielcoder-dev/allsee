8c04b3eca18f54b106040283f3bbc430986ec654
gabriel <s2gb77@gamil.com>
2025-09-29 19:52:30 -0400

    fix: styles ajust

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -17,7 +17,7 @@ export default function About() {
         <div>
           <p className="uppercase text-sm text-orange-600 font-bold tracking-wider mb-1">Sobre</p>
           <h2 className="text-3xl md:text-4xl font-extrabold text-black mb-4">ALLSEE</h2>
-          <p className="text-gray-500 mb-10">A ALL SEE é a maior empresa de mídia indoor e OOH de Primavera do Leste e uma referência em toda a região. Fundada por empresários com mais de 20 anos de experiência, destaca-se pela inovação, credibilidade e qualidade. Com tecnologia de ponta e telas digitais estrategicamente posicionadas, oferece soluções de comunicação que garantem visibilidade, engajamento e fortalecimento de marcas..</p>
+          <p className="text-gray-500 mb-10">A ALL SEE é a maior empresa de mídia indoor e OOH de Primavera do Leste e uma referência em toda a região. Fundada por empresários com mais de 20 anos de experiência, destaca-se pela inovação, credibilidade e qualidade. Com tecnologia de ponta e telas digitais estrategicamente posicionadas, oferece soluções de comunicação que garantem visibilidade, engajamento e fortalecimento de marcas.</p>
 
           <div className="space-y-6">
             {/* Item 1 */}
