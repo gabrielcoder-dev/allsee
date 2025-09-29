7aca9942b1eb2e88fca223555190965df9d83f94
gabriel <s2gb77@gamil.com>
2025-09-29 19:26:51 -0400

    fix: styles in pages newz

--- src/app/(public)/privacidade/page.tsx
+++ src/app/(public)/privacidade/page.tsx
@@ -7,7 +7,7 @@ export default function PrivacidadePage() {
     <div className="min-h-screen  bg-gray-50">
       <HeaderPolTerm />
       
-      <div className="max-w-4xl mx-auto px-4 pb-12 pt-32">
+      <div className="max-w-4xl mx-auto px-4 pb-12 pt-36">
         <div className="bg-white rounded-lg shadow-lg p-8">
           {/* Header */}
           <div className="text-center mb-12">
